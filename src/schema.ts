4a8200dc63c4499af4ed6176635937f8598f61ac
Natsune Mochizuki <me@natsuneko.cat>
2023-04-29 19:07:00 +0900

    chore: add reason lists

---
+++ src/schema.ts
@@ -0,0 +1,20 @@
+type Category =
+  | "affiliate" // アフィリエイトサイト
+  | "curation" // キュレーションサイト
+  | "machine-translation" // 機械翻訳転載サイト
+  | "meta" // メタ情報
+  | "news" // ニュースサイト
+  | "username"; // ユーザー投稿型サイト
+
+type Content = {
+  category: Category;
+  reasons: string[];
+};
+
+type Schema = {
+  blockings: {
+    [key: string]: Content;
+  };
+};
+
+export { Schema };
