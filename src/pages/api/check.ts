1acd88f798f613495476b4dc9f876a9ad6756559
Natsune Mochizuki <me@natsuneko.cat>
2023-05-03 20:28:38 +0900

    feat: add reason checker

---
+++ src/pages/api/check.ts
@@ -0,0 +1,48 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from "fs";
+import path from "path";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Schema } from "../../../schema/schema";
+
+const json = path.join(process.cwd(), "src", "data", "contents.json");
+
+const { blockings: items } = JSON.parse(
+  fs.readFileSync(json, "utf-8")
+) as Schema;
+
+type Data = {
+  domain: string;
+  category: string;
+  reasons: string[];
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { domain } = req.query;
+  if (!domain || Array.isArray(domain)) {
+    res.status(404).json({
+      domain: "unknown",
+      category: "unknown",
+      reasons: [],
+    });
+    return;
+  }
+
+  const item = items[domain];
+  if (!item) {
+    res.status(404).json({
+      domain: "unknown",
+      category: "unknown",
+      reasons: [],
+    });
+  }
+
+  res.status(200).json({
+    domain,
+    category: item.category,
+    reasons: item.reasons,
+  });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import React from "react";
+
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -49,6 +50,32 @@ const categories: Category[] = [
 ];
 
 export default function Home() {
+  const [domain, setDomain] = React.useState<string>("");
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [item, setItem] = React.useState<{
+    domain: string;
+    category: string;
+    reasons: string[];
+  } | null>(null);
+
+  const onChangeDomain = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDomain(e.target.value);
+
+    if (item && item.domain !== e.target.value) {
+      setItem(null);
+    }
+  };
+
+  const onCheckDomain = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsLoading(true);
+
+    const res = await fetch(`/api/check?domain=${domain}`);
+    const data = await res.json();
+    setItem(data);
+
+    setIsLoading(false);
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -70,15 +97,62 @@ export default function Home() {
             You can search for the reasons behind all the content through the
             following form.
           </p>
+          <form className="mt-8 flex place-items-center">
+            <input
+              type="text"
+              className="flex-grow h-12 text-black text-2xl px-2 rounded-sm appearance-none outline-none"
+              name="domain"
+              placeholder="natsuneko.cat"
+              defaultValue={domain}
+              onChange={onChangeDomain}
+              disabled={isLoading}
+            />
+            <button
+              type="submit"
+              className="h-12 w-36 ml-4 bg-white text-black rounded-sm text-2xl disabled:opacity-50"
+              onClick={onCheckDomain}
+              disabled={isLoading}
+            >
+              Check
+            </button>
+          </form>
+          {item && (
+            <div className="mt-4 w-full text-left max-w-[628px]">
+              {domain} is{" "}
+              {item.category !== "unknown" ? (
+                <span className="text-red-400">listed</span>
+              ) : (
+                <span className="text-green-500">unlisted</span>
+              )}
+              .{item.category !== "unknown" && <p>Category: {item.category}</p>}
+              {item.reasons.length > 0 && (
+                <div className="w-full overflow-hidden">
+                  Reasons:
+                  <ol className="list-inside pl-4 list-decimal mt-1">
+                    {item.reasons.map((w) => {
+                      return (
+                        <li
+                          className="text-ellipsis whitespace-nowrap overflow-hidden"
+                          key={w}
+                        >
+                          {w}
+                        </li>
+                      );
+                    })}
+                  </ol>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
 
-      <div className="mb-32 grid text-center gap-y-4 lg:mb-0  lg:grid-cols-[repeat(8,1fr)] lg:grid-rows-2 lg:gap-x-4 lg:text-left">
+      <div className="mb-32 mt-16 grid text-center gap-y-6 lg:mb-0 xl:grid-cols-[repeat(8,1fr)] xl:grid-rows-2 xl:gap-x-4 xl:text-left">
         {categories.map((w) => {
           return (
             <div
               key={w.link}
-              className="col-span-2 [&:nth-child(5)]:col-[3_/_span_2]"
+              className="col-span-2 xl:[&:nth-child(5)]:col-[3_/_span_2]"
             >
               <h3 className="text-3xl mb-2">{w.name}</h3>
               <p className="text-neutral-200 my-2">{w.description}</p>
