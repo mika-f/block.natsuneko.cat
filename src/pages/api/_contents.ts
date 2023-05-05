5f73119e8e3a1d436bb05a2620c3201ff9ca5263
Natsune Mochizuki <me@natsuneko.cat>
2023-05-05 17:09:12 +0900

    fix: fix symlink

--- src/pages/api/_contents.ts
+++ src/pages/api/_contents.ts
@@ -2,9 +2,9 @@ import fs from "fs";
 import { join } from "path";
 import YAML from "js-yaml";
 
-import { Schema } from "../../../schema/schema";
+import type { Schema } from "../../data/schema";
 
-const path = join(process.cwd(), "src", "data", "contents.yaml");
+const path = join(process.cwd(), "src", "data", "items.yaml");
 const contents = fs.readFileSync(path, "utf-8");
 const { blockings: items } = YAML.load(contents) as Schema;
 
